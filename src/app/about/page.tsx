93f14780f461f77825d136969a3dae857d6f3676
s0560996 <ododmengapa@gmail.com>
2025-04-17 10:12:35 +0200

    Fix about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <div className="m-2 p-7 max-w-xl mx-auto min-h-screen">
       <h1 className="text-2xl font-bold mb-4">About Me</h1>
-      <p>I'm a developer passionate about clean code and cool UI. Welcome to my About page!</p>
+      <p>I&#39m a developer passionate about clean code and cool UI. Welcome to my About page!</p>
     </div>
   );
 }
